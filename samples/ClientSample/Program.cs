4e6e2c1d8b816e702be906cdeec16551f9fc10c2
Mirko Da Corte <mirko@etherna.io>
2020-08-20 17:18:37 +0200

    First solution architecture

---
+++ samples/ClientSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Etherna.CreditClient.ClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
