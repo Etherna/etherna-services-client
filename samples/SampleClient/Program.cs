417cec2cccc1cf5f362ec04f95e602f6b267f9d8
Mirko Da Corte <mirko@etherna.io>
2020-09-01 00:25:32 +0200

    Implemented client

--- samples/SampleClient/Program.cs
+++ samples/SampleClient/Program.cs
@@ -1,13 +1,11 @@
-﻿using IdentityModel.Client;
-using System;
-using System.Net.Http;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace Etherna.CreditClient.SampleClient
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
             // Request Credit service info.
             Console.WriteLine("Etherna Credit base url:");
@@ -25,45 +23,11 @@ namespace Etherna.CreditClient.SampleClient
             var ssoClientSecret = Console.ReadLine();
             Console.WriteLine();
 
-            //*********
-            // discover endpoints from metadata
-            var httpClient = new HttpClient();
-            var disco = await httpClient.GetDiscoveryDocumentAsync(ssoBaseUrl);
-            if (disco.IsError)
-            {
-                Console.WriteLine(disco.Error);
-                return;
-            }
-            
-            // request token
-            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
-            {
-                Address = disco.TokenEndpoint,
-
-                ClientId = ssoClientId,
-                ClientSecret = ssoClientSecret,
-                Scope = "ethernaCredit_serviceInteract_api"
-            });
-
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
-
-            Console.WriteLine(tokenResponse.Json);
-
-            // call api
-            var apiClient = new HttpClient();
-            apiClient.SetBearerToken(tokenResponse.AccessToken);
-
-            //********
-
-            // Create service client.
-            ServiceInteractClient client = new ServiceInteractClient(baseUrl, apiClient);
+            // Create client.
+            var client = new EthernaCreditClient(new Uri(baseUrl), new Uri(ssoBaseUrl), ssoClientId, ssoClientSecret);
 
             // Consume service api.
-            var balance = await client.BalanceGetAsync(address);
+            var balance = await client.ServiceInteract.BalanceGetAsync(address);
             Console.WriteLine($"Current balance: ${balance}");
         }
     }
