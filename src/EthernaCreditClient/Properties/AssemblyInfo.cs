92026fbadcddf60d5b7025ad965d079b3b412e2a
Mirko Da Corte <mirko@etherna.io>
2021-05-02 19:44:56 +0200

    Updated c# language version Updated .net analizer Added license Updated compilation flags Removed Dispose diagnostic rules exceptions

--- src/EthernaCreditClient/Properties/AssemblyInfo.cs
+++ src/EthernaCreditClient/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿//   Copyright 2020-present Etherna Sagl
+//   Copyright 2020-present Etherna Sagl
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -12,10 +12,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-namespace Etherna.CreditClient
-{
-    public interface IServiceCreditClient
-    {
-        IServiceInteractClient ServiceInteract { get; }
-    }
-}
+using System;
+
+[assembly: CLSCompliant(false)]
