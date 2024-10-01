40d6f2bc844e9fd9bfd5767a7ebbda017bfc2a15
Mirko Da Corte <mirko@etherna.io>
2024-10-01 17:11:20 +0200

    fix license headers

--- src/EthernaSdk.Tools.Video/Models/VideoManifestCaptionSource.cs
+++ src/EthernaSdk.Tools.Video/Models/VideoManifestCaptionSource.cs
@@ -1,15 +1,15 @@
-// Copyright 2022-present Etherna SA
-// This file is part of Etherna Video Importer.
+// Copyright 2020-present Etherna SA
+// This file is part of Etherna SDK .Net.
 // 
-// Etherna Video Importer is free software: you can redistribute it and/or modify it under the terms of the
-// GNU Affero General Public License as published by the Free Software Foundation,
+// Etherna SDK .Net is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option) any later version.
 // 
-// Etherna Video Importer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// Etherna SDK .Net is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Affero General Public License for more details.
+// See the GNU Lesser General Public License for more details.
 // 
-// You should have received a copy of the GNU Affero General Public License along with Etherna Video Importer.
+// You should have received a copy of the GNU Lesser General Public License along with Etherna SDK .Net.
 // If not, see <https://www.gnu.org/licenses/>.
 
 using Etherna.BeeNet.Models;
