5f8fb5d7b52be452151ef04a7568791d76bf10b5
Mirko Da Corte <mirko@etherna.io>
2025-01-30 04:20:49 +0100

    update dependencies

--- src/EthernaSdk.Internal.AspNetCore/ServiceCollectionExtensions.cs
+++ src/EthernaSdk.Internal.AspNetCore/ServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Lesser General Public License along with Etherna SDK .Net.
 // If not, see <https://www.gnu.org/licenses/>.
 
-using IdentityModel.Client;
+using Duende.IdentityModel.Client;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
